8ad3ee4a40c6050e91f7ed90e015809927c6ff47
Olivia Smith <oliviaeleanaco@gmail.com>
2019-09-04 11:42:47 -0400

    first Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GITExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "olivia";
+            string lastname = "Smith";
+
+            int age = 30;
+
+            int oldage =  85;
+
+            int extraoldage = 100;
+
+            Console.WriteLine(firstname + " " + lastname);
+
+            Console.WriteLine("I am " + age + " years old");
+
+            Console.WriteLine("im really glad im not" + oldage + "years old");
+        }
+    }
+}
